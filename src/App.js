bf46d73ad578f8834f1cf3d47f886b401c47b9d1
Niharika Gurnani <niharikagurnani13@gmail.com>
2022-03-30 20:32:41 +0530

    Make a basic class  component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome/>
     </div>
   );
 }
